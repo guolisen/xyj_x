52b1e226cbf48f940a48d8370c6eb32c80f67d38
dayn9t <dayn9t@gmail.com>
2011-10-08 08:56:19 +0800

    npc

--- wiz/cvt-npc.c
+++ wiz/cvt-npc.c
@@ -2,11 +2,13 @@
 
 inherit NPC;
 
+#include <xyj_x.h>
+
 void create()
 {
 	object ob;
 
-	set_name("数据管理员", ({ "npc", }));
+	set_name("数据员", ({ "npc", }));
 
 	set("gender", "男性" );
 
@@ -15,10 +17,9 @@ void create()
 负责处理用户数据的NPC，也用下列指令：
 
     literate 等级                设定literate的新等级（只可用一次）
+    exchange                     用80K配额兑换潜能25K潜能
 
 ");
-
-
 	setup();
 	
 	carry_object("/obj/cloth");
@@ -28,11 +29,27 @@ void create()
 void init()
 {
 	add_action ("do_literate", "literate");
-
+	add_action ("do_exchange", "exchange");
 }
 
 
 int do_literate(string arg)
 {
 	return __DIR__"cvt"->set_literate(to_int(arg));
+}
+
+
+#define	NORM_RATIO			80
+
+int do_exchange(string arg)
+{
+	mapping rewards = ([PEXP : 25000]);
+
+	if(total_exp(_player) < 35*K) return notify_fail("你的武学道行太低！\n");
+
+	if(TASK_EXC->do_reward(_player, name(), "兑换", rewards, NORM_RATIO)) {
+		_player->add(PEXP, rewards[PEXP]);
+		tell_object(_player, "你兑换了潜能。\n");		
+	}
+	return 1;
 }
